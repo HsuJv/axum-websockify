74229048831bc6c2d0227de68f5b1644f0d6c3f6
Jovi Hsu <jv.hsu@outlook.com>
2022-09-23 02:22:54 +0000

    remove some debug code

--- src/agent.rs
+++ src/agent.rs
@@ -46,42 +46,4 @@ pub async fn handle_client(mut client: WebSocket) {
             },
         }
     }
-
-    // if let Some(msg) = client.recv().await {
-    //     if let Ok(msg) = msg {
-    //         match msg {
-    //             Message::Text(t) => {
-    //                 println!("client sent str: {:?}", t);
-    //             }
-    //             Message::Binary(_) => {
-    //                 println!("client sent binary data");
-    //             }
-    //             Message::Ping(_) => {
-    //                 println!("socket ping");
-    //             }
-    //             Message::Pong(_) => {
-    //                 println!("socket pong");
-    //             }
-    //             Message::Close(_) => {
-    //                 println!("client disconnected");
-    //                 return;
-    //             }
-    //         }
-    //     } else {
-    //         println!("client disconnected");
-    //         return;
-    //     }
-    // }
-
-    // loop {
-    //     if client
-    //         .send(Message::Text(String::from("Hi!")))
-    //         .await
-    //         .is_err()
-    //     {
-    //         println!("client disconnected");
-    //         return;
-    //     }
-    //     tokio::time::sleep(std::time::Duration::from_secs(3)).await;
-    // }
 }
